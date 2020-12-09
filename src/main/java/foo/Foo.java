d3b630ab1e61370aa5110c475a598af0e0010935
Rukmini268 <krukmini268@gmail.com>
2020-12-09 15:20:44 +0530

    changes done

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -7,6 +7,7 @@ public class Foo {
 
     public static int div(int a, int b) throws Exception {
     	if (b == 0) {
+            b==7
     		throw new UnsupportedOperationException("Can't divide by zero!");
     	}
         return a / b;  
